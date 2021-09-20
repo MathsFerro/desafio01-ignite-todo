1f4d4afc1953f80a9fe7c5b02f34ce7caecce1cc
Matheus Ferro <matheusferroraimundo@gmail.com>
2021-09-19 22:20:14 -0300

    get todo by username

--- src/index.js
+++ src/index.js
@@ -38,12 +38,13 @@ app.post('/users', (request, response) => {
   };
 
   users.push(user);
-  
+
   return response.status(201).send();
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
-  
+  const { user } = request;
+  return response.json(user.todos);
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
